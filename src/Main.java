e954a15d0b35696bdbc4d566edf534a316100b66
Josefine Anbo <josefineanbo@Josefines-MacBook-Air-2.local>
2019-09-24 08:58:35 +0200

    changed password

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "elinebidstrup";
-        String myPassword = "!2018HarSaxo";
+        String myPassword = "hej";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
