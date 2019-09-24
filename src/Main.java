c944573a25a6d8439ef8aa1f32429ea684786bd0
Josefine Anbo <josefineanbo@Josefines-MacBook-Air-2.local>
2019-09-24 09:21:18 +0200

    hej

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "elinebidstrup";
-        String myPassword = "hej";
+        String myPassword = "jose1";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
